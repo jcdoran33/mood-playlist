7908c2a40f57878226889994eaadcdb23709a26b
Jack Doran <jcdoran33@gmail.com>
2022-09-06 16:52:02 -0400

    created Login component

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+//import React
+import React from 'react';
+
+function Login() {
+
+    return (
+        <div className="App">
+
+        </div>
+    )
+};
+
+
+//export component "Login"
+export default Login;
