5845f2b3fd4d1fca5ee24237913aaf8e95bb798e
Jack Doran <jcdoran33@gmail.com>
2022-09-07 10:50:17 -0400

    finished webplayback component - having issue displaying Spotify player once login approved and user is authenticated

--- src/components/WebPlayback.jsx
+++ src/components/WebPlayback.jsx
@@ -1,11 +1,27 @@
 //import React, useState, useEffect
 import React, { useState, useEffect } from 'react';
 
+//define track as a JSON object
+const track = {
+    name: "",
+    album: {
+        images: [
+            { url: "" }
+        ]
+    },
+    artists: [
+        { name: "" }
+    ]
+};
+
 function WebPlayback(props) {
     const [player, setPlayer] = useState(undefined);
+    const [is_paused, setPaused] = useState(false);
+    const [is_active, setActive] = useState(false);
+    const [current_track, setTrack] = useState(track);
 
     //insert useEffect here
-    useEffect( () => {
+    useEffect(() => {
         const script = document.createElement("script");
         script.src = "https://sdk.scdn.co/spotify-player.js";
         script.async = true;
@@ -15,7 +31,7 @@ function WebPlayback(props) {
         window.onSpotifyWebPlaybackSDKReady = () => {
             const player = new window.Spotify.Player({
                 name: 'Web Playback SDK',
-                get0AuthToken: cb => { cb(props.token);},
+                get0AuthToken: cb => { cb(props.token); },
                 volume: 0.5
             });
 
@@ -29,8 +45,23 @@ function WebPlayback(props) {
                 console.log('Device ID has gone offline', device_id);
             });
 
+            player.addListener('player_state_changed', (state => {
+
+                if (!state) {
+                    return;
+                }
+
+                setTrack(state.track_window.current_track);
+                setPaused(state.paused);
+
+                player.getCurrentState().then(state => {
+                    (!state) ? setActive(false) : setActive(true)
+                });
+
+            }));
+
             player.connect();
-        
+
         };
     }, []);
 
@@ -39,7 +70,32 @@ function WebPlayback(props) {
         <>
             <div className="container">
                 <div className="main-wrapper">
+                    <img src={current_track.album.images[0].url}
+                        className="now-playing__cover" alt="" />
 
+                    <div className="now-playing__side">
+                        <div className="now-playing__name">{
+                            current_track.name
+                        }</div>
+
+                        <div className="now-playing__artist">{
+                            current_track.artists[0].name
+                        }</div>
+
+                        {/* Add playback control buttons here */}
+                        <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
+                            &lt;&lt;
+                        </button>
+
+                        <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
+                            {is_paused ? "PLAY" : "PAUSE"}
+                        </button>
+
+                        <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
+                            &gt;&gt;
+                        </button>
+
+                    </div>
                 </div>
             </div>
         </>
